1ec89f159fe6acde5cccec0bcf2fa28eb1ccc5f6
tracer-ics2020 <tracer@njuics.org>
2023-01-14 01:50:53 +0000

    >  compile 191220000    Static hostname: ip-172-31-11-115          Icon name: computer-vm            Chassis: vm         Machine ID: c7b3aabd9059440b8fdd94dae5074431            Boot ID: 5186559ca7dd4427b8e877898e8f11c1     Virtualization: xen   Operating System: Debian GNU/Linux 11 (bullseye)             Kernel: Linux 5.10.0-19-cloud-amd64       Architecture: x86-64  01:50:53 up 5 days, 18:35,  1 user,  load average: 0.05, 0.01, 0.00

--- nemu/include/isa/x86.h
+++ nemu/include/isa/x86.h
@@ -18,10 +18,15 @@
  */
 
 typedef struct {
-  struct {
-    uint32_t _32;
-    uint16_t _16;
-    uint8_t _8[2];
+  union {
+		uint32_t _32;
+		struct { 
+			uint16_t _pad;
+			union {
+				uint16_t _16;
+				uint8_t _8[2];
+			};
+		};
   } gpr[8];
 
   /* Do NOT change the order of the GPRs' definitions. */
